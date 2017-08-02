0cfbad9c1a82755033e76d4cc4e06abed67f611d
Guillem <info@disedit.com>
2017-08-02 14:06:25 +0200

    Top nav basic styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,10 @@
+<nav class="top-nav">
+  <a class="top-nav__parent-site" href="//compromis.net">
+    <img src="//compromis.net/wp-content/themes/Compromis/images/logo-compromis-retina.png" alt="Compromís" />
+  </a>
+  <a class="top-nav__site" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+</nav>
+
 <header class="banner">
   <div class="container">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
