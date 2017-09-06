2bb400781cbd313c1bdaa3db04b17bba278a4b45
Guillem <info@disedit.com>
2017-09-06 14:08:35 +0200

    Add header hero image placeholder

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,13 +5,16 @@
   <a class="top-nav__site" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
 </nav>
 
-<header class="banner">
+<div class="hero-image">
+  BACKGROUND IMAGE
+</div>
+
+<div class="nav-wrapper">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
   </div>
-</header>
+</div>
